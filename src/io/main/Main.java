de42aa99978b6a7d51a77525698624844e071c53
Felipe Cardim <73312771+FelipeQC1000@users.noreply.github.com>
2024-09-13 18:15:09 -0300

    Update Main.java

--- src/io/main/Main.java
+++ src/io/main/Main.java
@@ -10,17 +10,15 @@ public class Main {
             var finalArgs = new java.util.ArrayList<>(Arrays.stream(args).toList());
 
             if (finalArgs.isEmpty()) {
-                System.out.println("\nPor padrão, o programa roda gerando código em Java a partir do arquivo input.isi, e imprime o resultado no Terminal.");
-                System.out.println("Se deseja mudar este comportamento, adicione:");
-                System.out.println("  --target + 'java', 'c', 'interpret' ou 'repl' para gerar código de Java, C, rodar como interpretador, ou de forma interativa, respectivamente.");
-                System.out.println("  --input + <caminho do arquivo> para definir o código que o programa lê a partir de um arquivo.");
-                System.out.println("  --output + <caminho destino> para definir onde o código deve imprimir o resultado.");
-                System.out.println();
-
                 finalArgs.add("--target");
                 finalArgs.add("java");
                 finalArgs.add("--input");
                 finalArgs.add("input.isi");
+                System.out.println("\nPor padrão, o programa roda gerando código em Java a partir do arquivo input.isi, e imprime o resultado no Terminal. Se deseja mudar este comportamento, adicione:");
+                System.out.println("  --target + 'java', 'c', 'interpret' ou 'repl' para gerar código de Java, C, rodar como interpretador, ou de forma interativa, respectivamente.");
+                System.out.println("  --input + <caminho do arquivo> para definir o código que o programa lê a partir de um arquivo.");
+                System.out.println("  --output + <caminho destino> para definir onde o código deve imprimir o resultado.");
+                System.out.println();
             }
 
             ArgumentParser argumentParser = new ArgumentParser();
