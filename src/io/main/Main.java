1653d8a2c58ea5b748eb16d101a9983828d29347
Felipe Cardim <73312771+FelipeQC1000@users.noreply.github.com>
2024-09-13 16:19:02 -0300

    Update Main.java

--- src/io/main/Main.java
+++ src/io/main/Main.java
@@ -12,7 +12,7 @@ public class Main {
             if (finalArgs.isEmpty()) {
                 System.out.println("\nPor padrão, o programa roda gerando código em Java a partir do arquivo input.isi, e imprime o resultado no Terminal.");
                 System.out.println("Se deseja mudar este comportamento, adicione:");
-                System.out.println("  --target + 'java', 'c', 'interpret' ou 'REPL' para gerar código de Java, C, rodar como interpretador, ou de forma interativa, respectivamente.");
+                System.out.println("  --target + 'java', 'c', 'interpret' ou 'repl' para gerar código de Java, C, rodar como interpretador, ou de forma interativa, respectivamente.");
                 System.out.println("  --input + <caminho do arquivo> para definir o código que o programa lê a partir de um arquivo.");
                 System.out.println("  --output + <caminho destino> para definir onde o código deve imprimir o resultado.");
                 System.out.println();
