c3f9780112fcb04f592c9d6a11aad0915d9c87a4
Felipe Cardim <73312771+FelipeQC1000@users.noreply.github.com>
2024-09-13 18:35:06 -0300

    Update Main.java

--- src/io/main/Main.java
+++ src/io/main/Main.java
@@ -6,30 +6,22 @@ import io.mode.CLI;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            var finalArgs = new java.util.ArrayList<>(Arrays.stream(args).toList());
+        var Arguments = new java.util.ArrayList<>(Arrays.stream(args).toList());
 
-            if (finalArgs.isEmpty()) {
-                finalArgs.add("--target");
-                finalArgs.add("java");
-                finalArgs.add("--input");
-                finalArgs.add("input.isi");
-                System.out.println("\nPor padrão, o programa roda gerando código em Java a partir do arquivo input.isi, e imprime o resultado no Terminal. Se deseja mudar este comportamento, adicione:");
-                System.out.println("  --target + 'java', 'c', 'interpret' ou 'repl' para gerar código de Java, C, rodar como interpretador, ou de forma interativa, respectivamente.");
-                System.out.println("  --input + <caminho do arquivo> para definir o código que o programa lê a partir de um arquivo.");
-                System.out.println("  --output + <caminho destino> para definir onde o código deve imprimir o resultado.");
-                System.out.println();
-            }
-
-            ArgumentParser argumentParser = new ArgumentParser();
-            var arguments = argumentParser.parse(finalArgs);
-            CLI cli = new CLI(arguments.target, arguments.input, arguments.output);
-
-            cli.run();
-        } catch (Exception ex) {
-            System.err.println("Error: " + ex.getMessage());
-            System.out.println("Stack trace:");
-            ex.printStackTrace();
+        if (Arguments.isEmpty()) {
+            Arguments.add("--target");
+            Arguments.add("java");
+            Arguments.add("--input");
+            Arguments.add("input.isi");
+            System.out.println("\nPor padrão, o programa roda gerando código em Java a partir do arquivo input.isi, e imprime o resultado no Terminal. Se deseja mudar este comportamento, adicione:");
+            System.out.println("  --target + 'java', 'c', 'interpret' ou 'repl' para gerar código de Java, C, rodar como interpretador, ou de forma interativa, respectivamente.");
+            System.out.println("  --input + <caminho do arquivo> para definir o código que o programa lê a partir de um arquivo.");
+            System.out.println("  --output + <caminho destino> para definir onde o código deve imprimir o resultado.");
+            System.out.println();
         }
+        ArgumentParser argumentParser = new ArgumentParser();
+        var settings = argumentParser.parse(Arguments);
+        CLI cli = new CLI(settings.target, settings.input, settings.output);
+        cli.run();
     }
 }
